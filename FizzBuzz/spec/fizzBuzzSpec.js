cff29f540e0244dc92f8929ae8fe5ab1ae7a8885
ScottWoodbyrne <scott.woodbyrne@gmail.com>
2016-05-30 16:53:30 +0100

    update

--- FizzBuzz/spec/fizzBuzzSpec.js
+++ FizzBuzz/spec/fizzBuzzSpec.js
@@ -1,7 +1,49 @@
 describe("my function game", function() {
+	it("should exist", function()  {
+		expect(game).toBeDefined();
+	});
 
-    it("should exist", function()  {
-      expect(game).toBeDefined();
+	it("should return an array", function()  {
+
+		var result = game();
+		var isArray = ( result instanceof Array )
+
+		expect(isArray).toBeTruthy();
+	});   
+
+	it("should return an array of 100 elements", function()  {
+		var result = game();
+		expect(result.length).toEqual(100);	
+	});  
+
+	it("should return first element of array", function () {
+		var result = game();
+		expect(result[0]).toEqual(1);
+	})
+
+    it("should print fizz for number divisable by 3", function () {
+    	var result = game();
+		expect(result[8]).toEqual("fizz");
     });
+
+    it("should print fizz for numbers that contain 3", function () {
+    	var result = game();
+		expect(result[12]).toEqual("fizz");
+    });
+
+        it("should print buzz for number divisable by 5", function () {
+    	var result = game();
+		expect(result[9]).toEqual("buzz");
+    });
+
+   it("should print buzz for numbers that contain 5", function () {
+    	var result = game();
+		expect(result[24]).toEqual("buzz");
+    });
+   	it("should print fizzbuzz for numbers divisable by 3 AND 5", function () {
+    	var result = game();
+		expect(result[14]).toEqual("fizzbuzz");
+    });
+   
 
 });
--- FizzBuzz/src/fizzBuzz.js
+++ FizzBuzz/src/fizzBuzz.js
@@ -1,3 +1,30 @@
 function game() {
+	var output= [];
+	var isDivBy3;
+	var isDivBy5;
+	var has3InIt;
+	var has5InIt;
 
+
+	for (i =1; i<=100; i++) {
+
+		isDivBy3 = (i%3==0);
+		isDivBy5 = (i%5==0);
+		has3InIt = (i+"").indexOf("3")>-1;
+		has5InIt = (i+"").indexOf("5")>-1;
+
+		if (isDivBy3 && isDivBy5){
+			output.push("fizzbuzz")
+		}
+		else if (isDivBy3 || has3InIt){
+			output.push("fizz")	
+		}
+		else if (isDivBy5 || has5InIt){
+			output.push("buzz")
+		}
+		else {
+			output.push(i);
+		};
+	};
+	return output;
 };
